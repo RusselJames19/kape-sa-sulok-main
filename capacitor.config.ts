a9abdb49ecce4e3d0f5c484cdff7dade68f1f789
russeljamesagapito0-lgtm <russeljamesagapito0@gmail.com>
2026-05-08 19:22:25 +0800

    firstcommit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kapesasulok.inventory',
+  appName: 'Kape sa Sulok Inventory',
+  webDir: 'dist'
+};
+
+export default config;
